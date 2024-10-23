a5b42ee65fba73e0871fa7de90e740ec19d524e9
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2024-10-23 19:16:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 echo "Stiky's BSPWM dotfiles"
 
+sudo apt -y install bspwm rofi polybar sxhkd dunst cronie lf nemo qutebrowser mpd ncmpcpp
+
 cp -r ./config/* $HOME/.config/
 cp -r ./bin/* $HOME/.local/bin/
 
